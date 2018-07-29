10486b0a5a0ce41ed2dcaa1301e7797d311b6bf6
Ranchock <m15775966200@163.com>
2018-07-29 15:14:53 +0800

    英雄机类

---
+++ shootgame/Hero.java
@@ -0,0 +1,13 @@
+class Hero {
+    private static int life;
+    private static int doubleFire;
+    private static int speed;
+    Hero() {
+	super();
+	speed = 1;
+	life = 3;
+	doubleFire = 0;
+	}
+}
+
+
