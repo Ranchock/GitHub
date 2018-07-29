1ca06b23e581d659a3eeb0eee1793a555b4d55c5
Ranchock <m15775966200@163.com>
2018-07-29 15:03:13 +0800

    飞行物类，超类（包括天空、英雄机，敌机，蜜蜂，子弹）

---
+++ shootgame/FlyingObject.java
@@ -0,0 +1,15 @@
+class FlyingObject {
+	protected static int x;
+	protected static int y;
+	protected static int width;
+	protected static int height;
+	FlyingObject(int x,int y, int width,int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this,height = height;
+	}
+	FlyingObject(int width,int height) {
+		
+	}
+}
